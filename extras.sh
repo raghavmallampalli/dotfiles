6c72bb9e38219c30c4b21b1063a6d16ed061a674
Raghav <raghavmallampalli1234@gmail.com>
2025-12-22 00:36:52 +0530

    add imagemagick, make common to arch and ubuntu

--- extras.sh
+++ extras.sh
@@ -6,6 +6,14 @@ source "$(dirname "$0")/common.sh"
 
 trap 'cleanup ${LINENO} $?' EXIT
 
+# OS Detection
+OS_ID="unknown"
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    OS_ID=$ID
+fi
+
+log "INFO" "Detected OS: $OS_ID"
 # Collect all user inputs at the start
 if [ "$ROOT_MODE" = false ]; then
     if [ -z "$HAS_SUDO" ]; then
@@ -15,9 +23,23 @@ else
     HAS_SUDO="y"
 fi
 
-# FFMPEG
-if [[ $HAS_SUDO = y ]]; then
-    show_progress "Installing FFMPEG and related tools"
-    run_command apt-get install libhdf5-dev exiftool ffmpeg -y
-    finish_progress
+if [[ $OS_ID = "arch" ]]; then
+    if [[ $HAS_SUDO = y ]]; then
+        show_progress "Installing extra tools via yay"
+        execute yay -S --needed --noconfirm hdf5 perl-image-exiftool ffmpeg imagemagick ghostscript
+        finish_progress
+    fi
+
+elif [[ $OS_ID = "ubuntu" ]] || [[ $OS_ID = "debian" ]]; then
+
+    if [[ $HAS_SUDO = y ]]; then
+        # FFMPEG
+        show_progress "Installing FFMPEG and related tools"
+        run_command apt-get install libhdf5-dev exiftool ffmpeg -y
+        finish_progress
+        # ImageMagick and Ghostscript
+        show_progress "Installing ImageMagick and Ghostscript"
+        run_command apt-get install imagemagick ghostscript -y
+        finish_progress
+    fi
 fi
