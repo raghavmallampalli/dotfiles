87af9e154891aabe9519d40596a1271572f1183e
Raghav <raghavmallampalli1234@gmail.com>
2026-01-03 19:15:21 +0530

    minor improvements

--- dotfiles/nvim/.config/nvim/lua/plugins/user.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/user.lua
@@ -10,8 +10,24 @@ return {
   { "LazyVim/LazyVim", opts = { colorscheme = "catppuccin" } },
 
   -- Essential logic from your old setup not in LazyVim core
-  { "christoomey/vim-tmux-navigator" },
-  { "junegunn/vim-peekaboo" },
-
+  {
+    "christoomey/vim-tmux-navigator",
+    event = "VeryLazy"
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  }
   -- Import LazyVim extras (optional but recommended for your workflow)
 }
